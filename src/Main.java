c822b5718cac91021c8c7a2fac1d723f7075d350
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-15 17:44:57 +0300

    to10ok

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,21 +7,46 @@ public class Main {
         int count = in.nextInt();
         System.out.println("Введите числа:");
         double[] numbers = new double[count];
+        int[] deci = new int[count];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = in.nextInt();
-            while (numbers[i] >= 10) {
-                numbers[i] = numbers[i]/10;
+            if (numbers[i] % 10 == 0) {
+                while (numbers[i] % 10 == 0) {
+                    deci[i]++;
+                    numbers[i] = numbers[i] / 10;
+                }
+            } else {
+                while (numbers[i] >= 10) {
+                    numbers[i] = numbers[i] / 10;
+                }
             }
         }
-        Arrays.sort(numbers);
+        boolean sorted = false;
+        double temp;
+        int deciTemp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < numbers.length - 1; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    deciTemp = deci[i];
+                    deci[i] = deci[i + 1];
+                    deci[i + 1] = deciTemp;
+                    sorted = false;
+                }
+            }
+        }
         for (int i = 0; i < numbers.length; i++) {
             while (numbers[i] % 1 > 0) {
-                numbers[i] = numbers[i]*10;
+                numbers[i] = numbers[i] * 10;
             }
+            numbers[i] = numbers[i] * Math.pow(10, deci[i]);
         }
         System.out.println("Наименьшее составное число из введённых чисел: ");
-        for (double index:numbers) {
-            System.out.print((int)index);
+        for (double index : numbers) {
+            System.out.print((int) index);
         }
     }
 }
